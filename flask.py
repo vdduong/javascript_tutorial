cbd3b5161539b1fb0a4a343929d5ebeb9f4c27bc
vdduong <vdduong@free.fr>
2014-08-05 14:53:18 +0200

    Update flask.py

--- flask.py
+++ flask.py
@@ -7,3 +7,8 @@ def hello():
  
 if __name__ == "__main__":
     app.run()
+
+
+# Even if you don’t know Python very well this code shouldn’t be too difficult to understand. 
+# One route gets setup which if you hit the endpoint with a browser or cURL you will get the text Hello World 
+# returned back to you.
