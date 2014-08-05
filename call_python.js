41be40e28acdcbb81a98ccfa9e45d53a0157577a
vdduong <vdduong@free.fr>
2014-08-05 13:34:59 +0200

    Update call_python.js

--- call_python.js
+++ call_python.js
@@ -1,1 +1,8 @@
-// call jQuerry
+// learning jQuerry
+
+// show a message to visitors who use IE
+if($.browser.msie){
+  alert("C'est Internet Explorer " +$.browser.version + " !");
+}
+
+// jQuerry is not a language but a javascript library. Implying that JVscript to be activated within the browser.
