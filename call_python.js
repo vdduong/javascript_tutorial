b576c159957065768549b75326e5c8c08d62de86
vdduong <vdduong@free.fr>
2014-08-05 14:31:56 +0200

    Update call_python.js

--- call_python.js
+++ call_python.js
@@ -6,3 +6,27 @@ if($.browser.msie){
 }
 
 // jQuerry is not a language but a javascript library. Implying that JVscript to be activated within the browser.
+
+$("#AnyID").click(function())
+  {
+    $.ajax({
+      type: "get",
+      url: "<your-python-script>.py",
+      data: {"param1":"abc"},
+      async: false,
+      success: function(response){
+      // response is string, convert it to json and apply conditions.
+      var json_obj=eval('(' + response + ')'); // $.parseJSON("'"+response+"'");
+      if (json_obj.type == 'Error'){
+        alert(json_obj.msg);
+      }
+      else {
+        alert(json_obj.msg);
+      } // else closed
+      }, //success closed
+      error:function(xhr, err)
+      {
+        alert("Error connecting to server, please contact system administrator.")
+      }
+    }) // ajax closed
+  }
